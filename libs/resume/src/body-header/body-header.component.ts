87a2a2de6d0df0a6993f5a5baff2dc0eb7532310
Gautamatul <gautamatul662@gmail.com>
2019-10-26 08:41:57 +0530

    6 files modified

--- libs/resume/src/body-header/body-header.component.ts
+++ libs/resume/src/body-header/body-header.component.ts
@@ -9,13 +9,15 @@ import moment from 'moment';
 export class BodyHeaderComponent implements OnInit {
 
   joiningDate = '2017-12-06';
-  totalYears: number;
+  totalYears: number = 2;
   totalMonths: number;
 
   constructor() { }
 
   ngOnInit() {
-    this.totalYears = moment().diff(moment(this.joiningDate).subtract(1, 'months'), 'years')+;
+    // this.totalYears = 
+    // moment().diff(moment(this.joiningDate).subtract(1, 'months'), 'years');
+
     // this.totalMonths = (moment().diff(moment(this.joiningDate).subtract(1, 'months'), 'months') % 12);
     console.log(this.totalYears);
     console.log(this.totalMonths);
